76a9270b74ebd580a985fffbfd07e9c1bd0412cf
Md. Shahed Hossain Chowdhury <shahedtrishan51@gmail.com>
2023-10-04 19:56:00 +0600

    day_six_folder_upload

---
+++ day_6/Hurdle_1.py
@@ -0,0 +1,23 @@
+def three_left():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def full_move():
+    move()
+    turn_left()
+    move()
+    three_left()
+    move()
+    three_left()
+    move()
+    turn_left()
+
+
+full_move()
+full_move()
+full_move()
+full_move()
+full_move()
+full_move()
---
+++ day_6/function.py
@@ -0,0 +1,9 @@
+def first_function():
+    print("Hello")
+    print("Mom!")
+    print("How")
+    print("are")
+    print("you?")
+
+
+first_function()
